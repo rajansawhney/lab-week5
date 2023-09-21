cff0e72198183aa961515d56fe1d52f77b070eeb
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-09-20 20:52:44 -0700

    Initial commit

---
+++ admission.c
@@ -0,0 +1,29 @@
+/*
+Objective of Week 4 Lab assignment
+
+1. Practice using if else for one problem.
+2. Practice using switch case break for the other.
+3. And anything else that was covered in the class so far.
+
+Write a C program that determines a candidate's eligibility for admission to an advanced course based on the following criteria:
+
+Eligibility Criteria:
+1. Grades in Maths >=65 and Grades in Physics >=55 and Grades in Chemistry>=50 and
+2. Total in all three subject >=190 or Total in Maths and Physics >=140.
+
+Sample Inputs:
+Input the marks obtained in Physics: 65 
+Input the marks obtained in Chemistry: 51 
+Input the marks obtained in Mathematics: 72 
+Total marks of Maths, Physics and Chemistry: 188 
+Total marks of Maths and Physics: 137 
+
+Expected Output:
+The candidate is not eligible for admission.
+
+NOTE: 
+1. Read input **only** in the following order: physics, chemistry, math.
+2. Output should **exactly** say either "The candidate is eligible for admission." or "The candidate is not eligible for admission."
+3. Input should be of type integer only.
+4. If you used if/else statements for the prob1, use switch/case statements for prob2.
+*/
